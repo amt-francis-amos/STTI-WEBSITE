e858ff184048bda5c520e4e2a0b76305cd98b587
francis.amos123 <fmamos@st.nsbt.edu.gh>
2023-09-12 14:42:30 +0000

    commit to main

---
+++ script.js
@@ -0,0 +1,20 @@
+// ---- MENU BAR ------
+
+
+let show = document.getElementById("nav-links")
+
+  
+function showMenu(){
+   show.style.right = "0";
+ 
+}
+
+function closeMenu(){
+   show.style.right = "-200px";
+
+}
+
+
+
+
+
