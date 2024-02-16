77f7e72347103e38d7419256f3c90de12a0b0861
francis amos <francis.amos@amalitech.org>
2024-02-16 00:14:47 +0000

    commit to main

--- script.js
+++ script.js
@@ -17,3 +17,53 @@ function closeMenu(){
 
 
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+   var overlay = document.getElementById("overlay");
+   overlay.style.display = "flex";
+ 
+
+   window.addEventListener("load", function () {
+     setTimeout(function () {
+       overlay.style.display = "none";
+     }, 4000); 
+   });
+ });
+ 
+
+
+
+
+function scrollToTop() {
+
+   function scrollToTopAnimation() {
+     var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+ 
+     if (currentPosition > 0) {
+       window.requestAnimationFrame(scrollToTopAnimation);
+       window.scrollTo(0, currentPosition - currentPosition / 8);
+     }
+   }
+ 
+   scrollToTopAnimation();
+ }
+ 
+
+ window.addEventListener("scroll", function () {
+   var scrollButton = document.getElementById("scroll-to-top");
+ 
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     scrollButton.style.display = "block";
+   } else {
+     scrollButton.style.display = "none";
+   }
+ });
+ 
+
+
+ 
+
+
+
